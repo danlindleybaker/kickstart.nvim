2f0fdbacba011820cabb3794526e44689edbd20d
danlindleybaker <danlindleybaker@gmail.com>
2023-10-23 12:02:07 +0100

    lowered updatetime to 100ms

--- init.lua
+++ init.lua
@@ -172,7 +172,8 @@ vim.wo.number = true
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
-
+-- change update time 
+vim.o.updatetime = 100
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
